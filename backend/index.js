98412d77a878e83cf2a24109bc44dfd9982f3b8c
nesrin <nesrinnoufal99@gmail.com>
2024-03-29 07:24:03 +0530

    express server and mongodb setup,model creation,routes

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import express from "express"
+import connectToMongodb from "./db/connectToMongodb.js";
+import AuthRoutes from './routes/auth.routes.js';
+import cors from 'cors';
+import path from "path";
+
+
+const app = express ();
+const PORT = 5000;
+
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cors({origin: 'http://localhost:3000'}));
+
+app.use('/api/auth',AuthRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
+
+// app.get('/', () =>{
+    
+//     console.log("Server running ");
+// })
+
+app.listen(PORT, () => {
+    connectToMongodb();
+    console.log(`Server running at port ${PORT}`);
+});
